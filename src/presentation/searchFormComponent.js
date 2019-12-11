cdd025aece3c7aef5097f74a92780c38e02ec0c1
Sadeq Gholami <sadeqg@kth.se>
2019-12-11 21:18:28 +0100

    some changes for better user interaction

--- src/presentation/searchFormComponent.js
+++ src/presentation/searchFormComponent.js
@@ -3,7 +3,11 @@ const SearchFormComponent=({buttonClicked, dishTypeChanged, searchTextChanged})=
         return (  
         <React.Fragment>
             <h2>Find A Dish</h2>
-            <input type="text" defaultValue={sessionStorage.getItem("searchText")} onChange={e => searchTextChanged(e.target.value) }/>
+            <input type="text" defaultValue={sessionStorage.getItem("searchText")} 
+            onChange={e => searchTextChanged(e.target.value) }
+            onKeyPress={e=> {
+                        let key = e.which || e.keyCode;
+                         if(key===13) buttonClicked(e)}}/>
             <select defaultValue = {sessionStorage.getItem("dishType")}onChange={ e => dishTypeChanged(e.target.value)}>
                 <option value ="">all</option>
                 <option value="Main Course" >Main Course</option>
