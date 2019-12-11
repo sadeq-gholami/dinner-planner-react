c24f8abe975c0ab49aa87b0789dfce8da09a9e67
Sadeq Gholami <sadeqg@kth.se>
2019-12-11 15:48:31 +0100

    some changes in style

--- src/presentation/searchFormComponent.js
+++ src/presentation/searchFormComponent.js
@@ -4,12 +4,13 @@ const SearchFormComponent=({buttonClicked, dishTypeChanged, searchTextChanged})=
         <React.Fragment>
             <h2>Find A Dish</h2>
             <input type="text" onChange={e => searchTextChanged(e.target.value) }/>
-            <select onChange={ e => dishTypeChanged(e.target.value)}>
+            <select defaultValue = "all" onChange={ e => dishTypeChanged(e.target.value)}>
+                <option value ="">all</option>
                 <option value="Main Course" >Main Course</option>
                 <option value="Side Dish" >Side Dish</option>
                 <option value="Desert" >Desert</option>
                 <option value="Appetizer" >Appetizer</option>
-                <option value ="">...</option>
+                
             </select>
             <button className={"btn"} onClick={buttonClicked}>Search</button>
             <hr/>
